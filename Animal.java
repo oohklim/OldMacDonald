999ef8bccfb95ff8e9e0e3ce1532bf48a1ed1b0c
unknown <kelim3@697-334-eowyn.sf.k12.internal>
2015-10-21 10:37:55 -0700

    done

--- Animal.java
+++ Animal.java
@@ -1,3 +1,4 @@
 interface Animal {    
- //yourcodehere
+  public String getSound();        
+  public String getType(); 
 }  
--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,9 @@
-class NamedCow
+class NamedCow extends Cow
 {
-
+	private String myName;
+	NamedCow(String type, String name, String sound)
+	{
+		myName = name;
+	}
+	public String getName(){return myName;}
 }
